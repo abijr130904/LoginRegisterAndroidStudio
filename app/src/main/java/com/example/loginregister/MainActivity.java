5d8d162202cf71a86e3357b05f325cc44f0517cf
Moh.habiburrohman <abijr130904@gmail.com>
2024-09-02 15:31:51 +0700

    ADD: connect firebase

--- app/src/main/java/com/example/loginregister/MainActivity.java
+++ app/src/main/java/com/example/loginregister/MainActivity.java
@@ -1,24 +1,85 @@
 package com.example.loginregister;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.loginregister.register.register_Activity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
 public class MainActivity extends AppCompatActivity {
+    EditText inputEmail, inputPassword;
+    String email, password;
+    Button btnLogin;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        inputEmail = findViewById(R.id.edt_email_login);
+        inputPassword = findViewById(R.id.edt_password_login);
+        btnLogin = findViewById(R.id.btn_login);
+        TextView txv_signup = findViewById(R.id.txv_signup);
+
+        txv_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigasi ke halaman register
+                Intent intent = new Intent(MainActivity.this, register_Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cekLogin();
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void cekLogin() {
+        email = inputEmail.getText().toString();
+        password = inputPassword.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                         Toast.makeText(MainActivity.this, "Login Berhasil", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Login Gagal,email atau password anda salah", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/loginregister/register/register_Activity.java
+++ app/src/main/java/com/example/loginregister/register/register_Activity.java
@@ -1,26 +1,96 @@
 package com.example.loginregister.register;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.loginregister.MainActivity;
 import com.example.loginregister.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class register_Activity extends AppCompatActivity {
+    EditText  inputemail, inputpassword, inputconfirmpassword;
+    String  email, password, confirmpassword;
+    Button btnregister;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        inputemail = findViewById(R.id.edt_email_register);
+        inputpassword = findViewById(R.id.edt_password_register);
+        inputconfirmpassword = findViewById(R.id.edt_confirmpassword_register);
+        btnregister = findViewById(R.id.button_register);
+        TextView txv_signin = findViewById(R.id.txv_signin);
+
+        txv_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigasi ke halaman register
+                Intent intent = new Intent(register_Activity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cekRegister();
+            }
+        });
+    }
+
+    private void cekRegister() {
+        email = inputemail.getText().toString();
+        password = inputpassword.getText().toString();
+        confirmpassword = inputconfirmpassword.getText().toString();
+
+        if (!password.equals(confirmpassword)) {
+            Toast.makeText(register_Activity.this, "Password dan Konfirmasi Password tidak cocok", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(register_Activity.this, "Register Berhasil", Toast.LENGTH_SHORT).show();
+                            //navigasi
+                            Intent intent = new Intent(register_Activity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish(); // Memastikan pengguna tidak bisa kembali ke activity register
+                        } else {
+                            String errorMessage = task.getException().getMessage();
+                            Toast.makeText(register_Activity.this, "Register Gagal, email atau password Anda salah"+ errorMessage, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
